a21c981b07619f1bdde0a086606015372d93d9fc
driss-khelfi <driss.khelfi@laplateforme.io>
2024-07-08 17:26:48 +0200

    chat console

--- index.js
+++ index.js
@@ -4,10 +4,37 @@ const app = require("express")();
 //creation serveur 
 const http = require("http").createServer(app);
 
+//instanciation socket.io
+const io = require("socket.io")(http);
+
 //creation de la route slash
 app.get("/",(req, res)=>{
     res.sendFile(__dirname + "/index.html");
 });
+
+//ecoute de l'évènement connexion socket.io
+io.on("connection", (socket) => {
+    console.log("une connexion s'active");
+    //console.log(socket);
+
+//ecoute de la deconnexion
+    socket.on("disconnect",() => {
+        console.log("un utilisateur s'est déconnecté");
+    });
+
+    //gestion du chat
+socket.on("chat_message",(msg)=>{
+    console.log(msg);
+    //relai du message vers tous les utilisateurs conencté
+    io.emit("chat_message", msg);
+});
+
+});
+
+
+
+
+
 
 //interrogation du serveur sur le port 3000
 http.listen(3000, ()=>{
